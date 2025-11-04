54aa67dbf3f34a76b0039e577c15a29b5b2b526b
Ashwini <panianu8@gmail.com>
2025-11-04 12:32:56 +0530

    second

--- env_pro/env_pro/settings.py
+++ env_pro/env_pro/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env=environ.env()
+environ.env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,9 @@ WSGI_APPLICATION = 'env_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':env('NAME'),
+        'PASSWORD':env("PASS")
     }
 }
 
